082ffbfa0cea32931757676676ddf595245c185e
Vikki <gorshaya-viktoria@mail.ru>
2020-07-19 07:09:36 +0300

    3 v

--- untitled5/wsgi.py
+++ untitled5/wsgi.py
@@ -11,6 +11,8 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'untitled5.settings')
-
+if os.environ.get('DJANGO_ENV') == 'production':
+        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'untitled5.production')
+    else:
+        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'untitled5.settings')
 application = get_wsgi_application()
